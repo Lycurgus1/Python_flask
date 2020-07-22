0a2a3c5805271ff23806048cd205beaa82f30c29
Lycurgus1 <max.palmer.official@outlook.com>
2020-07-22 16:46:43 +0100

    22/07/2020 v3

--- flask_project/README.md
+++ flask_project/README.md
@@ -3,10 +3,22 @@ This is a project to develop flask
 **Iteration 1**
 - Code with log in functionality
 - Base HTML template
+- ***DONE***
 
 **Iteration 2**
+- Create index functionality
+- Emojis and extras in body
+- ***DONE***
+
+**Full Project**
+1. Person logs in
+2. Questions on website
+3. User input taken to python
+4. Answers put in CSV file
+
+**Try and include**
 - Inherit html template in another template
 - Create nav bar
 
-**Iteration 3**
-- Emojis and extras in body
+
+
--- flask_project/app.py
+++ flask_project/app.py
@@ -4,8 +4,13 @@ from flask import Flask, render_template, redirect, url_for, request
 # Create flask instance
 app = Flask(__name__)
 
+# Create index method
+@app.route('/')
+def index():
+    return render_template('base.html')
+
 # Create log in method
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -13,7 +18,7 @@ def login():
             error = 'Invalid Credentials. Please try again.'
         else:
             return "Success!"
-    return render_template('base.html', error=error)
+    return render_template('log_in.html', error=error)
 
 # Run app
 if __name__ == "__main__":
